cd1f2510bd7d9c6e9d47b495b9444831f8f01b36
muraooyuri <yuri.0112hm@icloud.com>
2023-06-26 05:52:46 +0000

    ビューの編集とコントローラ記述

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,21 @@
 class UserController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
+  before_action :authenticate_user!
+  before_action :check_guest_user, only: [:edit]
+  
+    def index
+      @user = current_user
+      @genres = Genre.all
+    end
+  
+    def show
+      @user = User.find(params[:id])
+      @genres = Genre.all
+    end
+  
+    def edit
+      @user = User.find(params[:id])
+      if @user != current_user
+        redirect_to user_path(current_user)
+      end
+    end
 end
